ee2847d929be3aad1851cc6587d268515ca80ca3
Steve Bilogan <steve.bilogan@gmail.com>
2024-03-12 08:05:48 -0400

    update

--- ExtendedSplashApp/App.cs
+++ ExtendedSplashApp/App.cs
@@ -20,10 +20,8 @@ public class App : Application
 		// just ensure that the window is active
 		if (MainWindow.Content is not Shell shell)
 		{
-			// Create a Frame to act as the navigation context and navigate to the first page
 			shell = new Shell();
 
-			// Place the frame in the current Window
 			MainWindow.Content = shell;
 
 			shell.RootFrame.NavigationFailed += OnNavigationFailed;
@@ -34,7 +32,6 @@ public class App : Application
 			// When the navigation stack isn't restored navigate to the first page,
 			// configuring the new page by passing required information as a navigation
 			// parameter
-			///await Task.Delay(5000);
 			shell.RootFrame.Navigate(typeof(MainPage), args.Arguments);
 		}
 
